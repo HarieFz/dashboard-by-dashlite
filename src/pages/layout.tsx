cf7bf010815552ec1022f74a559183b6a2c7a96c
HarieFz <hfairuzzaki@gmail.com>
2025-05-13 15:26:44 +0700

    fix/layout: responsive desktop and laptop

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout() {
   return (
     <div className="relative min-h-dvh font-source-sans-pro">
       <Sidebar />
-      <div className="w-[calc(100%-286px)] ms-auto bg-[#F4F4F4]">
+      <div className="desktop:w-[calc(100%-286px)] laptop:w-[calc(100%-200px)] ms-auto bg-[#F4F4F4]">
         <Header />
         <main className="max-w-9xl mx-auto px-9 py-6 min-h-dvh">
           <Outlet />
